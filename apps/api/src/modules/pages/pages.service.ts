87ee16cadfdf8d35959f19e1ed3601c0272b64eb
Aakash Priyadarshi <aakashm301@gmail.com>
2026-05-11 11:40:54 +0530

    chore: auto-format pages.service.ts

--- apps/api/src/modules/pages/pages.service.ts
+++ apps/api/src/modules/pages/pages.service.ts
@@ -1,7 +1,7 @@
 import {
-  ConflictException,
-  Injectable,
-  NotFoundException,
+    ConflictException,
+    Injectable,
+    NotFoundException,
 } from "@nestjs/common";
 import { PrismaService } from "../../prisma/prisma.service";
 import { CreatePageDto, UpdatePageDto } from "./dto/page.dto";
